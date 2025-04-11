8238ea066868dfc91f857d6e6b1cdbc87d60631c
fzodd111 <fsostavkin@gmail.com>
2025-04-11 18:38:28 +0200

    Update server.py

--- GLITCH_web/server.py
+++ GLITCH_web/server.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 import logging
 import json
@@ -60,4 +61,5 @@ def get_cities(req):
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
